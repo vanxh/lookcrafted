9ca99e306ef628ec34cc90740d4e1f82899639ee
Vanxh <hello@vanxh.dev>
2025-05-02 05:04:01 +0400

    fix vercel server

--- apps/server/api/index.ts
+++ apps/server/api/index.ts
@@ -1,10 +1,49 @@
+import "dotenv/config";
+import { RPCHandler } from "@orpc/server/fetch";
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { logger } from "hono/logger";
 import { handle } from "hono/vercel";
-import app from "../src/index";
+import { auth } from "./lib/auth";
+import { createContext } from "./lib/context";
+import { appRouter } from "./routers/index";
 
-const handler = handle(app);
+const app = new Hono();
 
-export const GET = handler;
-export const POST = handler;
-export const PATCH = handler;
-export const PUT = handler;
-export const OPTIONS = handler;
+app.use(logger());
+app.use(
+	"/*",
+	cors({
+		origin: process.env.CORS_ORIGIN?.split(",") || [],
+		allowMethods: ["GET", "POST", "OPTIONS"],
+		allowHeaders: ["Content-Type", "Authorization"],
+		credentials: true,
+	}),
+);
+
+app.on(["POST", "GET"], "/api/auth/**", (c) => auth.handler(c.req.raw));
+
+const handler = new RPCHandler(appRouter);
+app.use("/rpc/*", async (c, next) => {
+	const context = await createContext({ context: c });
+	const { matched, response } = await handler.handle(c.req.raw, {
+		prefix: "/rpc",
+		context: context,
+	});
+	if (matched) {
+		return c.newResponse(response.body, response);
+	}
+	await next();
+});
+
+app.get("/", (c) => {
+	return c.text("OK");
+});
+
+const vercelHandler = handle(app);
+
+export const GET = vercelHandler;
+export const POST = vercelHandler;
+export const PATCH = vercelHandler;
+export const PUT = vercelHandler;
+export const OPTIONS = vercelHandler;
