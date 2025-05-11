4b05a4f52a5fa2ba8dad3b5db84d64273d7c8317
Vanxh <hello@vanxh.dev>
2025-05-11 04:21:15 +0400

    drizzle update

--- apps/server/src/db/index.ts
+++ apps/server/src/db/index.ts
@@ -2,6 +2,7 @@ import { drizzle } from "drizzle-orm/neon-http";
 
 import { env } from "../env";
 import * as schema from "./schema";
+
 export const db = drizzle(env.DATABASE_URL ?? "", {
 	schema,
 });
